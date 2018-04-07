7493b74db9131e65bac2c750249969ff582ab3c6
Theo <ntheo919@gmail.com>
2018-04-07 14:53:42 -0700

    add files

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import *
+from flask_socketio import *
+import os
+app = Flask(__name__, static_folder="static")
+app.config["SECRET_KEY"] = 'secret!'
+socket = SocketIO(app)
+@socket.on('connected')
+def connected():
+    print('recived message: 123abc')
+    
+@app.route("/")
+def index():
+    return render_template('SpaceGame.html')
+
+if __name__ == "__main__":
+        print("sever start")
+        print("client loaded")
+        port = int(os.environ.get("PORT", 5000))
+        socket.run(app, debug=False, host='0.0.0.0', port=port)
